20c622bbaa329c38000796b5f9efde2e725d6520
tomer bibi <tomerbibi2002@gmail.com>
2021-03-10 16:01:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Threaded_Workers_34_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> queue = new List<int>
+            {
+                4,
+                6,
+                7,
+                4,
+                9,
+                8,
+                23,
+                37,
+                2
+            };
+            Workers w = new Workers();
+            w.Pull(queue);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Workers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multi_Threaded_Workers_34_
+{
+    class Workers
+    {
+        private List<Task> tasks = new List<Task>(5);
+
+        public void Pull(List<int> ints)
+        {
+            ints.ForEach(_ =>
+            {
+                if (tasks.Count < 6)
+                    tasks.Add((Task.Run(() => Console.WriteLine(_ * 2))));
+                else
+                {
+                    bool done = false;
+                    while (!done)
+                    {
+                        for (int i = 0; i < 5; i++)
+                        {
+                            if(tasks[i].IsCompleted)
+                            {
+                                tasks[i] = ((Task.Run(() => Console.WriteLine(_ * 2))));
+                                done = true;
+                                break;
+                            }
+                            if(i == 4)
+                                Thread.Sleep(100);
+                        }
+                    }
+                }
+            });
+        }
+
+        public Workers()
+        {
+            
+        }
+    }
+}
